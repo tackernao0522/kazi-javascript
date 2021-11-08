5e3cc056a02a0ee2ee25f02c40e45d13b7608629
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-08 14:42:41 +0900

    関数型(基本)

---
+++ Function-Types/index.js
@@ -0,0 +1,23 @@
+// JavaScript Function
+
+function showMyName(name) {
+    console.log('My name is ' + name);
+    // console.log('My name is Takaki');
+}
+showMyName('Takaki');
+showMyName('Naomi');
+showMyName('Mieko');
+showMyName('Kaira');
+
+// let num1 = 30;
+// let num2 = 20;
+// let sum = num1 + num2;
+// console.log(sum); // 50
+
+function calcSum(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+const result = calcSum(30, 20);
+console.log(result); // 50
