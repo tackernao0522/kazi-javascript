1fb8e944563e416db9951e14948c0b0a9b29c8d0
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-08 13:05:32 +0900

    参照型オブジェクト

---
+++ Object-Types/index.js
@@ -0,0 +1,18 @@
+let name = 'Ariyan';
+let age = 40;
+let hobby = 'reading';
+
+let user = {
+    name: 'Ariyan',
+    age: 40,
+    hobby: 'reading'
+}
+
+console.log(user); // {name: 'Ariyan', age: 40, hobby: 'reading'}
+console.log(user.name); // Ariyan
+console.log(user.age); // 40
+console.log(user.hobby); // reading
+
+console.log(user['name']); // Ariyan
+console.log(user['age']); // 40
+console.log(user['hobby']); // reading
