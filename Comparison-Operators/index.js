2b0e5ff26e05c0bec445d893861fc7defe3d9839
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-09 12:14:44 +0900

    比較演算子

---
+++ Comparison-Operators/index.js
@@ -0,0 +1,13 @@
+// Comparison Operators
+// > < >= <= === !==
+
+let price = 200;
+
+console.log(price > 300); // false
+console.log(price >= 200); // true
+console.log(price < 100); // false
+console.log(price <= 500); // true
+console.log(price === 500); // false
+console.log(price === 200); // true
+console.log(price !== 200); // false
+console.log(price !== 500); // true
