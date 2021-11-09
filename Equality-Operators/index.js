d34c3dd80567bf6a83850fb8cb8a34904dcdad96
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-09 12:56:37 +0900

    等式演算子

---
+++ Equality-Operators/index.js
@@ -0,0 +1,9 @@
+// Eqiality Operators
+
+let price = 10;
+
+console.log(price === 10); // true
+console.log(price !== 20); // true
+
+console.log(price == '10'); // true
+console.log(price != 20); // true
