0fa706165f2c1713ff9e6770dc6dd55a27eaa6e0
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-09 11:57:52 +0900

    代入演算子

---
+++ Assignment-Operators/index.js
@@ -0,0 +1,25 @@
+// Assignment Operators
+
+// let num1 = 10;
+// let num2 = num1;
+
+// console.log(num1, num2); // 10 10
+
+let num1 = 10;
+let num2 = num1 + 5;
+num1 += 5;
+console.log(num2); // 15
+
+num1 = num1 - 5;
+num1 -= 5;
+
+num1 = num1 * 5;
+num1 = 5;
+
+num1 = num1 + 1;
+num1 += 1;
+num1++;
+
+num1 = num1 - 1;
+num1 -= 1;
+num1--;
