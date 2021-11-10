b263bd0fc7680741c96bf2ec835870fb8bb15688
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-10 19:04:45 +0900

    While loop

---
+++ While-loop/index.js
@@ -0,0 +1,14 @@
+// While loop
+
+let index = 1;
+
+while (index <= 4) {
+    console.log('Hi Ariyan', index);
+    index++;
+}
+
+index = 5;
+while(index >= 1) {
+    console.log('Hi Kazi', index);
+    index--;
+}
