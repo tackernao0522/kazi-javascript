d87a807de4c441c432a2eb06d48ac8de5b06c68f
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-10 11:32:28 +0900

    三項演算子

---
+++ Ternary-Operators/index.js
@@ -0,0 +1,16 @@
+// Conditional Statements
+// Ternary Operator
+
+let number1 = 10;
+let number2 = 2;
+
+let maxValue;
+if (number1 > number2) {
+    maxValue = number1;
+} else {
+    maxValue = number2;
+}
+
+let max = number1 > number2 ? number1 : number2; // Ternary Oprerator
+console.log(maxValue); // 10
+console.log(max); // 10
