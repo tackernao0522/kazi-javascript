2997b64f38b98fb1fefc910bb554ae339ee8b09e
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-10 21:19:07 +0900

    Nested Loops

---
+++ Infinite-loops/index.js
@@ -0,0 +1,22 @@
+// Nested loop (入れ子d)
+
+for (let ariyan = 1; ariyan <= 3; ariyan++) {
+    for (let kazi = 1; kazi <= 3; kazi++) {
+        console.log('ariyan: ' + ariyan + ' ' + 'kazi: ' + kazi);
+    }
+    // console.log('ariyan: ' + ariyan);
+}
+
+/*
+1 1
+1 2
+1 3
+
+2 1
+2 2
+2 3
+
+3 1
+3 2
+3 3
+*/
