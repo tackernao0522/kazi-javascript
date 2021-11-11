f807b37cabad1e2e3b9a459c66922fa5512e3b50
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-11 12:55:08 +0900

    String Methods(CharAt, toLocaleUpperCase, toLocaleLowerCase, includes, startWith, endsWith)

---
+++ String-Methods01/index.js
@@ -0,0 +1,13 @@
+let message = 'Hi My name Is Ariyan';
+
+console.log(message); // Hi My name Is Ariyan
+console.log(message.charAt(1)); // i
+console.log(message.toLocaleUpperCase()); // HI MY NAME IS ARIYAN
+console.log(message.toLocaleLowerCase()); // hi my name is ariyan
+console.log(message.includes('Ariyan')); // true
+console.log(message.includes('Kazi')); // false
+console.log(message.startsWith('Hi')); // true
+console.log(message.startsWith('Hirr')); // false
+console.log(message.endsWith('Ariyan')); // true
+console.log(message.endsWith('Ariyandd')); // false
+console.log(message.endsWith('My')); // false
