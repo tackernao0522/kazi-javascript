bee18e57af0461875e2638b51c72d4d50c14b3e1
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-11 13:15:03 +0900

    String Methods(Search, indexOf, lastIndexOf)

---
+++ String-Methods02/index.js
@@ -0,0 +1,7 @@
+let message = 'Hi My name Is Hi Ariyan';
+console.log(message);
+console.log(message.search('Hi')); // 0
+console.log(message.indexOf('Hi')); // 0
+console.log(message.search('name')); // 6
+console.log(message.indexOf('name')); // 6
+console.log(message.lastIndexOf('Hi')); // 14
