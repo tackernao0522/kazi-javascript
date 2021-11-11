52d281a9a9b87d9b83a5795a2b9eefb8125b72c0
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-11 13:30:30 +0900

    String Methods(toString, Concat, split)

---
+++ String-Methods03/index.js
@@ -0,0 +1,14 @@
+let message = 'Hi My name Is Hi Ariyan';
+let temp = [1, 2, 3, 4];
+
+let m1 = 'HI';
+let m2 = ' My name';
+let m3 = ' Is Ariyan';
+
+let m = m1.concat(m2, m3);
+console.log(m); // HI My name Is Ariyan
+
+console.log(typeof temp.toString()); // string
+console.log(temp.toString()); // 1,2,3,4
+
+console.log(message.split(' ')); // ['Hi', 'My', 'name', 'Is', 'Hi', 'Ariyan']
