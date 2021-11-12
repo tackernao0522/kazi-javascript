cf42d726368431b83972778abc2660e016c31ce2
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-12 16:35:30 +0900

    オブジェクトの書き方

---
+++ Object-Basics/index.js
@@ -0,0 +1,18 @@
+// Object Basics
+
+let name = 'Kazi';
+let age = 40;
+let hobby = 'Reading';
+
+const user = {
+    name: 'Kazi',
+    age: 40,
+    hobby: 'Reading'
+}
+console.log(user); // {name: 'Kazi', age: 40, hobby: 'Reading'}
+console.log(user.name); // Kazi
+console.log(user['name']); // Kazi
+console.log(user.age); // 40
+console.log(user['age']); // 40
+console.log(user.hobby) // Reading
+console.log(user['hobby']); // Reading
